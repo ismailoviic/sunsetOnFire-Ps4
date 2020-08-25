e5e5152012f99cc78f88b462186c180f5f0a65d5
Ismail Essabbagh <45835323+ismailoviic@users.noreply.github.com>
2020-08-25 17:23:57 +0100

    Updates

---
+++ precache-manifest.086e4a802506040a18c706220cfd64f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d91da88681210fa38298c7f7cec9d0",
+    "url": "/sunsetOnFire-Ps4/index.html"
+  },
+  {
+    "revision": "4bb05a675cd4c8ab07e7",
+    "url": "/sunsetOnFire-Ps4/static/css/main.9fbc2ec8.chunk.css"
+  },
+  {
+    "revision": "e3363490b90afeef185c",
+    "url": "/sunsetOnFire-Ps4/static/js/2.2ef7cdde.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sunsetOnFire-Ps4/static/js/2.2ef7cdde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb05a675cd4c8ab07e7",
+    "url": "/sunsetOnFire-Ps4/static/js/main.9ba11ff4.chunk.js"
+  },
+  {
+    "revision": "b800cd5d30ffebbbdae3",
+    "url": "/sunsetOnFire-Ps4/static/js/runtime-main.6e174b91.js"
+  },
+  {
+    "revision": "4932085e2f95cd89798b674a2764d5d0",
+    "url": "/sunsetOnFire-Ps4/static/media/pslogo.4932085e.png"
+  },
+  {
+    "revision": "041513fdb973ae04cb97acf4c9fa425e",
+    "url": "/sunsetOnFire-Ps4/static/media/soflogo.041513fd.png"
+  }
+]);
